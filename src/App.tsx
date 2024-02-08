680d81b7036750098757d818bd9ae86343f824f4
Thato Will Joseph <programergeek@gmail.com>
2024-02-08 11:54:09 +0200

    feat: set default camera position

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,17 @@
-import { Canvas } from "@react-three/fiber";
+import { Canvas, useThree } from "@react-three/fiber";
 import "./App.css";
 import { OrbitControls } from "@react-three/drei";
 import * as THREE from "three";
+import { BoardComponent } from "./components/board";
+import "reactflow/dist/style.css";
 
 function App() {
   return (
     <div className="grid grid-cols-2">
       <div id="canvas-container" className="h-screen w-full">
         <Canvas scene={{ background: new THREE.Color("rgb(66,66,66)") }}>
-          <perspectiveCamera />
+          <Camera />
+          <perspectiveCamera position={new THREE.Vector3().set(20, 20, 20)} />
           <ambientLight intensity={1} />
           <OrbitControls />
           <mesh>
@@ -17,8 +20,19 @@ function App() {
           </mesh>
         </Canvas>
       </div>
+      <div>
+        <BoardComponent />
+      </div>
     </div>
   );
 }
 
 export default App;
+
+const Camera = () => {
+  const { camera } = useThree();
+
+  camera.position.set(20, 20, 20);
+
+  return <camera />;
+};
