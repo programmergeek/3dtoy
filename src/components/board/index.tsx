ef7d9aba1dd57e99478a0d0ce8efc0501588b388
Thato Will Joseph <programergeek@gmail.com>
2024-02-08 11:55:18 +0200

    feat: add flowboard component

---
+++ src/components/board/index.tsx
@@ -0,0 +1,35 @@
+import { useCallback } from "react";
+import ReactFlow, {
+  Connection,
+  Edge,
+  addEdge,
+  useEdgesState,
+  useNodesState,
+} from "reactflow";
+
+const initialNodes = [
+  { id: "1", position: { x: 0, y: 0 }, data: { label: "1" } },
+  { id: "2", position: { x: 0, y: 100 }, data: { label: "2" } },
+];
+const initialEdges = [{ id: "e1-2", source: "1", target: "2" }];
+
+export const BoardComponent: React.FC = () => {
+  const [nodes, setNodes, onNodesChange] = useNodesState(initialNodes);
+  const [edges, setEdges, onEdgesChange] = useEdgesState(initialEdges);
+
+  const onConnect = useCallback(
+    (params: Edge | Connection) => setEdges((eds) => addEdge(params, eds)),
+    [setEdges],
+  );
+  return (
+    <div className="h-screen w-full">
+      <ReactFlow
+        nodes={nodes}
+        edges={edges}
+        onNodesChange={onNodesChange}
+        onEdgesChange={onEdgesChange}
+        onConnect={onConnect}
+      />
+    </div>
+  );
+};
