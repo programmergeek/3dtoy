4b52dd47fe7c667907b30e39d62e015e852703c8
Thato Will Joseph <programergeek@gmail.com>
2024-02-08 08:16:26 +0200

    feat: create viewport

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Canvas } from "@react-three/fiber";
+import "./App.css";
+import { OrbitControls } from "@react-three/drei";
+import * as THREE from "three";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="grid grid-cols-2">
+      <div id="canvas-container" className="h-screen w-full">
+        <Canvas scene={{ background: new THREE.Color("rgb(66,66,66)") }}>
+          <perspectiveCamera />
+          <ambientLight intensity={1} />
+          <OrbitControls />
+          <mesh>
+            <boxGeometry args={[10, 10, 10]} />
+            <meshStandardMaterial />
+          </mesh>
+        </Canvas>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
